5ad779ab150991569083db9fb31b5c1ecb9039c9
andrewhamilton <andrew.hamilton@exelisinc.com>
2016-02-24 14:33:12 -0500

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by ahamilton on 2/24/16.
+ */
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+io.on('connection', function(client){
+  console.log('Client connected...');
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(8080);
